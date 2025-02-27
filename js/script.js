1279353fb025811ae4a0e123988eb95f7331c8ed
Rhuan Lucas <rhuanlucassbs@gmail.com>
2025-02-27 10:39:17 -0300

    Final optimization

--- js/script.js
+++ js/script.js
@@ -1,25 +1,67 @@
-// Ativar links do menu
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-    const url = location.href;
-    const href = link.href;
-    if (url.includes(href)) {
-        link.classList.add("ativo");
-    }
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
 
-// Ativar itens orçamento
+// Ativar Items do Orçamento
 
 const parametros = new URLSearchParams(location.search);
 
-function ativarProdutos(parametro) {
-    const element  = document.getElementById(parametro);
-    if (element) {
-        element.checked = true;
-    }
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
-parametros.forEach(ativarProdutos);
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
